988496ee4f4edb379fab3f5b70196146262208c4
Mairbek Madiev <138805519+madiew1@users.noreply.github.com>
2025-09-14 09:34:11 +0200

    I have developed a component of the search engine, an API for it, as well as receiving and displaying products from the database

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import Image from 'next/image';
 import {Button} from '../ui';
 import {User, ShoppingCart, ArrowRight} from "lucide-react";
+import Link from 'next/link';
+import { SearchInput } from './search-input';
 
 interface Props {
   className?: string;
@@ -15,14 +17,19 @@ export const Header: React.FC<Props> = ({ className }) => {
       <Container className='flex items-center justify-between py-8'>
 
         {/* Левая часть */}
+       <Link href="/">
         <div className="flex item-center gap-4">
-            <Image src="/logo.png" alt="Logo" width={50} height={40}/>
-            <div>
-              <h1 className="text-2xl uppercase font-black">Empire Sneakers</h1>
-              <p className="text-sm text-gray-400 leading-3">Лучше уже некуда</p>
-            </div>
-        </div>
+              <Image src="/logo.png" alt="Logo" width={50} height={40}/>
+              <div>
+                <h1 className="text-2xl uppercase font-black">Empire Sneakers</h1>
+                <p className="text-sm text-gray-400 leading-3">Лучше уже некуда</p>
+              </div>
+          </div>
+       </Link>
 
+      <div className="mx-10 flex-1">
+        <SearchInput/>
+      </div>
         {/* Правая часть */}
 
         <div className="flex items-center gap-3">
